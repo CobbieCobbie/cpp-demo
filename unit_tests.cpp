775c2908eeb0ed8cefc7f1242a1f438922121fa9
cobbiecobbie <Benjamin.Coban@gmail.com>
2021-11-24 17:36:40 +0100

    Adapted unit tests for Triangle class and equality operator

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -2,9 +2,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 #include "Rectangle.hpp"
+#include "Triangle.hpp"
 #include <iostream>
 
-TEST_CASE("Succeeding unit tests")
+TEST_CASE("Rectangle unit tests")
 {
     Rectangle rect;
     CHECK(rect.get_area() == 0);
@@ -12,13 +13,25 @@ TEST_CASE("Succeeding unit tests")
     CHECK(rect.get_area() == 100);    
 }
 
-TEST_CASE("Unit tests with sub cases"){
-    Rectangle rect;
+TEST_CASE("Triangle unit tests"){
+    Triangle t1, t2;
     SUBCASE("Not setting the area"){
-        CHECK(rect.get_area() == 0);
+        CHECK(t1.get_area() == 0);
+        CHECK(t2.get_area() == 0);
+        CHECK(t1 == t2);
     }
-    SUBCASE("Setting the area"){
-        rect.set_values(10,10);
-        CHECK(rect.get_area() == 100);
+    SUBCASE("Setting the area for one triangle"){
+        t1.set_values(3,4,5);
+        CHECK(t1.get_area() == 6);
+        CHECK(! (t1 == t2));
+    }
+    SUBCASE("Setting both areas")
+    {
+        t1.set_values(3,4,5);
+        t2.set_values(5,4,3);
+        CHECK(t1.get_area() == 6);
+        CHECK(t2.get_area() == t1.get_area());
+        CHECK(t1 == t2);
+    
     }
 }
